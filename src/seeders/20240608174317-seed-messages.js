14522925c136eb49593bb7ab8b2820e9f78d3e86
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-08 15:54:46 -0400

    feat: add seeders

--- src/seeders/20240608174317-seed-messages.js
+++ src/seeders/20240608174317-seed-messages.js
@@ -1,25 +1,95 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  up: async (queryInterface) => queryInterface.bulkInsert('Messages', [
+    {
+      content: 'Hi, how are you?',
+      chatId: 1,
+      userId: 1,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'I am fine, thank you',
+      chatId: 1,
+      userId: 2,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'What are you doing?',
+      chatId: 1,
+      userId: 1,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'I am working',
+      chatId: 1,
+      userId: 2,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'Hi, how are you?',
+      chatId: 2,
+      userId: 1,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'I am fine, thank you',
+      chatId: 2,
+      userId: 2,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'What are you doing?',
+      chatId: 2,
+      userId: 1,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'I am working',
+      chatId: 2,
+      userId: 2,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'What are you talking without me?',
+      chatId: 2,
+      userId: 3,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      content: 'Read the previous messages',
+      chatId: 2,
+      userId: 1,
+      state: 'sent',
+      date: new Date(),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+  ]),
+  down: async (queryInterface) => queryInterface.bulkDelete('Messages', null, {}),
 };
