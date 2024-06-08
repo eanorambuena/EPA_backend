d89d51da84a404bad86c36e2302bc239f0d79749
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-08 18:09:25 -0400

    fix: added statuses to routes

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -2,29 +2,42 @@ const Router = require('koa-router');
 
 const router = new Router();
 
-router.get('', async (ctx) => {
-    const messages = await ctx.state.db.Message.findAll();
-    ctx.body = messages;
+router.get("messages.list",'', async (ctx) => {
+    try {
+        const messages = await ctx.state.db.Message.findAll();
+        ctx.body = messages;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error
+        ctx.status = 500;
+    }
     });
 
-router.get('/:chatid', async (ctx) => {
-    const messages = await ctx.state.db.Message.findAll({
-        where: {
-            chatId: ctx.params.chatid
-        }
-    });
-    ctx.body = messages;
+router.get("messages.chatlist",'/:chatid', async (ctx) => {
+    try {
+        const messages = await ctx.state.db.Message.findAll({
+            where: {
+                chatId: ctx.params.chatid
+            }
+        });
+        ctx.body = messages;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error;
+        ctx.status = 500;
+    }
 });
 
-router.post('', async (ctx) => {
-    const message = await ctx.state.db.Message.create({
-        chatId: ctx.params.chatId,
-        content: ctx.request.body.content,
-        userId: ctx.request.body.userId,
-        state: 'sent',
-        date: new Date(),
-    });
-    ctx.body = message;
+router.post("messages.create",'', async (ctx) => {
+    try {
+        const message = await ctx.state.db.Message.create(ctx.request.body);
+        ctx.body = message;
+        ctx.status = 201;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error;
+        ctx.status = 400;
+    }
 });
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,40 +3,74 @@ const Router = require('koa-router');
 const router = new Router();
 
 router.get("users.list", "/", async (ctx) => {
-    const users = await ctx.orm.User.findAll();
-    ctx.body = users;
+    try {
+        const users = await ctx.orm.User.findAll();
+        ctx.body = users;
+        ctx.status = 200;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error;
+        ctx.status = 500;
+    }
     });
 
 router.get("users.show", "/:id", async (ctx) => {
-    const user = await ctx.orm.User.findByPk(ctx.params.id);
-    if (!user) {
-        ctx.throw(404);
+    try {
+        const user = await ctx.orm.User.findByPk(ctx.params.id);
+        if (!user) {
+            ctx.throw(404);
+        }
+        ctx.body = user;
+        ctx.status = 200;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error;
+        ctx.status = 500;
     }
-    ctx.body = user;
     });
 
 router.post("users.create", "", async (ctx) => {
-    const user = await ctx.orm.User.create(ctx.request.body);
-    ctx.body = user;
+    try {
+        const user = await ctx.orm.User.create(ctx.request.body);
+        ctx.body = user;
+        ctx.status = 201;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error;
+        ctx.status = 400;
+    }
     });
 
 router.put("users.update", "/:id", async (ctx) => {
-    const user = await ctx.orm.User.findByPk(ctx.params.id);
-    if (!user) {
-        ctx.throw(404);
+    try {
+        const user = await ctx.orm.User.findByPk(ctx.params.id);
+        if (!user) {
+            ctx.throw(404);
+        }
+        await user.update(ctx.request.body);
+        ctx.body = user;
+        ctx.status = 200;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error;
+        ctx.status = 500;
     }
-    await user.update(ctx.request.body);
-    ctx.body = user;
     }
 );
 
 router.del("users.delete", "/:id", async (ctx) => {
-    const user = await ctx.orm.User.findByPk(ctx.params.id);
-    if (!user) {
-        ctx.throw(404);
+    try {
+        const user = await ctx.orm.User.findByPk(ctx.params.id);
+        if (!user) {
+            ctx.throw(404);
+        }
+        await user.destroy();
+        ctx.status = 204;
+    } catch (error) {
+        console.log(error);
+        ctx.body = error;
+        ctx.status = 500;
     }
-    await user.destroy();
-    ctx.status = 204;
     }
 );
 
