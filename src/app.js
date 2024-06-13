0dec8e989a15d92aa23fa971c978664ff7f26fb2
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-13 02:10:28 -0400

    feat: Add @koa/cors dependency and enable CORS middleware in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const KoaLogger = require('koa-logger')
 const { koaBody } = require('koa-body')
+const cors = require('@koa/cors')
 const router = require('./routes.js')
 const orm = require('./models')
 
@@ -8,6 +9,7 @@ const app = new Koa()
 
 app.context.orm = orm
 
+app.use(cors())
 app.use(KoaLogger())
 app.use(koaBody())
 
