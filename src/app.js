8b265f551676f23e89aad1f1f8ce8a260d313349
Pabloleiva00 <paleiva5@uc.cl>
2024-06-13 21:19:16 -0400

    fix: delete duplicate cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const { koaBody } = require('koa-body')
 const cors = require('@koa/cors')
 const router = require('./routes.js')
 const orm = require('./models')
-const cors = require('@koa/cors');
 
 const app = new Koa()
 
