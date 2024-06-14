84d46d978a0466430113d0f74f4a703ee69c5595
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-14 10:17:33 -0400

    feat: implement bcrypt

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router')
 const { User } = require('../models')
 const { assertRequiredFields } = require('../services/assertRequiredFields')
+const bcrypt = require('bcrypt');
 const Safely = require('../services/safely')
 const { sendJwt } = require('../services/createJwt')
 
@@ -11,8 +12,13 @@ router.post('/', async ctx => Safely.Do(ctx, async (ctx) => {
   const { phoneNumber, password } = ctx.request.body
   console.log('phoneNumber', phoneNumber)
   console.log('password', password)
-  const user = await User.findOne({ where: { phoneNumber, password } })
-  if (!user) {
+
+  const user = await User.findOne({ where: { phoneNumber: phoneNumber } })
+
+  const validPassword = await bcrypt.compare(password,user.password)
+
+  
+  if (!validPassword) {
     ctx.body = { error: 'Invalid credentials' }
     ctx.status = 401
     return
--- src/seeders/20240608173416-seed-users.js
+++ src/seeders/20240608173416-seed-users.js
@@ -1,22 +1,24 @@
+const bcrypt = require('bcrypt');
+
 module.exports = {
   up: async queryInterface => queryInterface.bulkInsert('Users', [
     {
       phoneNumber: '56912345678',
-      password: 'safePassword',
+      password: await bcrypt.hash('safePassword',10),
       type: 'admin',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       phoneNumber: '56987654321',
-      password: 'safePassword',
+      password: await bcrypt.hash('safePassword6',10),
       type: 'user',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       phoneNumber: '56944445678',
-      password: 'safePassword',
+      password: await bcrypt.hash('safePassword3',10),
       type: 'user',
       createdAt: new Date(),
       updatedAt: new Date()
