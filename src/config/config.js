1ee5534608696a245ab50fa82f6c7eee99c705bc
Pabloleiva00 <paleiva5@uc.cl>
2024-06-13 20:24:35 -0400

    fix: name of bdd development

--- src/config/config.js
+++ src/config/config.js
@@ -32,7 +32,7 @@ module.exports = {
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
     dialectOptions: {
