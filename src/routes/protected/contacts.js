1666d7e2c76ae1297e83d7e3419534a43b007901
MaxAl100 <43099597+MaxAl100@users.noreply.github.com>
2024-06-14 19:24:34 -0400

    feat: added contact routes which users can use

--- src/routes/protected/contacts.js
+++ src/routes/protected/contacts.js
@@ -20,8 +20,27 @@ router.get('/:id', isAdmin, async ctx => Safely.Do(ctx, async (ctx) => {
   ctx.status = 200
 }))
 
+router.get('/user/:id', async ctx => Safely.Do(ctx, async (ctx) => {
+  const contacts = await ctx.orm.Contact.findAll({
+    where: {
+      userBase: ctx.params.id
+    }
+  })
+  ctx.body = contacts
+  ctx.status = 200
+}))
+
 router.post('/', isAdmin, async ctx => Safely.Do(ctx, async (ctx) => {
   const contact = await ctx.orm.Contact.create(ctx.request.body)
+  ctx.body = contact
+  ctx.status = 201
+}))
+
+router.post('/user/:id', async ctx => Safely.Do(ctx, async (ctx) => {
+  const contact = await ctx.orm.Contact.create({
+    ...ctx.request.body,
+    userBase: ctx.params.id
+  })
   ctx.body = contact
   ctx.status = 201
 }))
@@ -36,8 +55,37 @@ router.put('/:id', isAdmin, async ctx => Safely.Do(ctx, async (ctx) => {
   ctx.status = 200
 }))
 
+router.put('/user/:id/', async ctx => Safely.Do(ctx, async (ctx) => {
+  const contact = await ctx.orm.Contact.findOne({
+    where: {
+      userBase: ctx.params.id,
+      userContact: ctx.request.body.userContact
+    }
+  })
+  if (!contact) {
+    throw new ItemNotFoundException('Contact')
+  }
+  await contact.update(ctx.request.body)
+  ctx.body = contact
+  ctx.status = 200
+}))
+
 router.del('/:id', isAdmin, async ctx => Safely.Do(ctx, async (ctx) => {
   const contact = await ctx.orm.Contact.findByPk(ctx.params.id)
+  if (!contact) {
+    throw new ItemNotFoundException('Contact')
+  }
+  await contact.destroy()
+  ctx.status = 204
+}))
+
+router.del('/user/:id', async ctx => Safely.Do(ctx, async (ctx) => {
+  const contact = await ctx.orm.Contact.findOne({
+    where: {
+      userBase: ctx.params.id,
+      userContact: ctx.request.body.userContact
+    }
+  })
   if (!contact) {
     throw new ItemNotFoundException('Contact')
   }
