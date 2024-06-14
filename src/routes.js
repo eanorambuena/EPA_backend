31280f818b4be3085276d39781f7877b1b44caf2
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-14 18:38:57 -0400

    feat: Add profiles routes and CRUD operations

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ const Contacts = require('./routes/protected/contacts.js')
 const Chats = require('./routes/protected/chats.js')
 const Me = require('./routes/protected/me.js')
 const Messages = require('./routes/protected/messages.js')
+const Profiles = require('./routes/protected/profiles.js')
 const jwtMiddleware = require('koa-jwt')
 
 const router = new Router()
@@ -23,6 +24,7 @@ router.use('/chats', Chats.routes())
 router.use('/contacts', Contacts.routes())
 router.use('/me', Me.routes())
 router.use('/messages', Messages.routes())
+router.use('/profiles', Profiles.routes())
 router.use('/users', Users.routes())
 
 module.exports = router
--- src/routes/protected/profiles.js
+++ src/routes/protected/profiles.js
@@ -1,5 +1,42 @@
 const Router = require('koa-router')
+const Safely = require('../../services/safely')
+const { isAdmin } = require('../../middleware/jwt')
+const { ItemNotFoundError } = require('../../services/errors')
+const { assertRequiredFields } = require('../../services/assertRequiredFields')
 
 const router = new Router()
+
+router.get('/', isAdmin, async ctx => Safely.Do(ctx, async (ctx) => {
+  const profiles = await ctx.orm.Profile.findAll()
+  ctx.body = profiles
+  ctx.status = 200
+}))
+
+router.get('/:userId', async ctx => Safely.Do(ctx, async (ctx) => {
+  const profile = await ctx.orm.Profile.findOne({ where: { userId: ctx.params.userId } })
+  if (!profile) {
+    console.log('Profile not found userId:', ctx.params.userId)
+    throw new ItemNotFoundError('Profile')
+  }
+  ctx.body = profile
+  ctx.status = 200
+}))
+
+router.patch('/:userId', async ctx => Safely.Do(ctx, async (ctx) => {
+  const profile = await ctx.orm.Profile.findOne({ where: { userId: ctx.params.userId } })
+  if (!profile) {
+    throw new ItemNotFoundError('Profile')
+  }
+  await profile.update(ctx.request.body)
+  ctx.body = profile
+  ctx.status = 200
+}))
+
+router.post('/', async ctx => Safely.Do(ctx, async (ctx) => {
+  await assertRequiredFields(ctx.request.body, ['username', 'email', 'status', 'description', 'image', 'userId'])
+  const profile = await ctx.orm.Profile.create(ctx.request.body)
+  ctx.body = profile
+  ctx.status = 201
+}))
 
 module.exports = router
