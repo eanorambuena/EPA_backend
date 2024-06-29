b99ab04474018617e4f3e418001c23f21c0c4e73
Emmanuel Norambuena <eanorambuena@uc.cl>
2024-06-28 22:40:25 -0400

    refactor: Update socket.io server configuration for real-time messaging

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 const app = require('./app')
 const db = require('./models')
 const dotenv = require('dotenv')
+const { Server } = require('socket.io')
+const { createServer } = require('node:http')
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
-const httpServer = require('http').createServer(app.callback())
+const server = createServer(app.callback())
 const options = {
   cors: {
     origin: 'http://localhost:5173',
     methods: ['GET', 'POST']
-  }
+  },
+  connectionStateRecovery: {}
 }
-const io = require('socket.io')(httpServer, options)
+const io = new Server(server, options)
 
 io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
 
-  socket.on('add_message', (msg) => {
+  socket.on('add_message', async (msg) => {
     console.log('message: ' + JSON.stringify(msg))
-    io.emit('add_message', msg)
+    io.emit('new_message', msg)
   })
 })
 
@@ -30,7 +33,7 @@ db.sequelize
   .authenticate()
   .then(() => {
     console.log('Connection has been established successfully.')
-    httpServer.listen(PORT)
+    server.listen(PORT)
     console.log(`Server running on http://localhost:${PORT}`)
   })
   .catch((err) => {
